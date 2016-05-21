f2778113a426dec063e7a278a45f18967a1573b0
Andy <andrew.john.orourke@gmail.com>
2016-05-21 12:43:17 +1000

    Created the asgn2Tests package with accompanying test cases

---
+++ AircraftSimulator/src/asgn2Tests/A380Tests.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Andrew O'Rourke
+ *
+ */
+public class A380Tests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ AircraftSimulator/src/asgn2Tests/BusinessTests.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Elliott Moore
+ *
+ */
+public class BusinessTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
