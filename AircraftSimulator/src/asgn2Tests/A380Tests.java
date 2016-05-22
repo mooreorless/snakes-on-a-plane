e8d604732544fdfa2540441cac010871534e58c0
Andy <andrew.john.orourke@gmail.com>
2016-05-22 17:49:01 +1000

    Test changes

--- AircraftSimulator/src/asgn2Tests/A380Tests.java
+++ AircraftSimulator/src/asgn2Tests/A380Tests.java
@@ -18,22 +18,12 @@ import java.util.List;
 
 public class A380Tests {
 
-	private A380 populatedA380;
-	private List<Passenger> seats;
-	private Economy testPassenger1;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		populatedA380 = new A380("Test Object", 2000, 10, 10, 10, 10);
-		seats.add(testPassenger1);
-		System.out.println(populatedA380.getNumEconomy());
-	}
-	
-	@Test
-	public void test() throws AircraftException{
-		assertNotNull(populatedA380);
+		
 	}
 
 	// Test methods for A380/Aircraft constructor
@@ -89,17 +79,13 @@ public class A380Tests {
 //		testA380.cancelBooking(testPassenger, 6);
 //	}
 	
-//	@Test
-//	public void testUpgrade() throws AircraftException, PassengerException{
-//		
-//		A380 testA380 = new A380("testFlightCode", 2200);
-//		Economy testPassenger = new Economy(2, 4);
-//		int beforeValue = testA380.getNumPremium();
-//		testPassenger.upgrade();
-//		int afterValue = testA380.getNumPremium();
-//		assertEquals(beforeValue, afterValue);
-//		System.out.println(beforeValue);
-//		System.out.println(afterValue);
-//	}
+	@Test
+	public void testUpgrade() throws AircraftException, PassengerException{
+		Economy testPassenger = new Economy(2, 4);
+		String beforeValue = testPassenger.getPassID();
+		testPassenger.upgrade();
+		String afterValue = testPassenger.getPassID();
+		assertFalse(beforeValue, afterValue);
+	}
 	
 }
